2f70ba6beb7e8ed50473e71a3b1749eeb5235a4f
Piotr Machacki <piotr@machacki.pl>
2023-06-05 09:55:43 +0200

    refactor: Move game logic from Scene to separate component

---
+++ src/components/GameComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <container>
+        <PlayerEntity ref="playerElement" />
+        <FoodsEntity ref="foodsElement" />
+    </container>
+</template>
+
+
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
+import { Rectangle } from 'pixi.js';
+import { useApplication } from 'vue3-pixi';
+import { storeToRefs } from 'pinia';
+import { useStore } from '@/store';
+import { GameState } from '@/types';
+
+import PlayerEntity from '@/entities/PlayerEntity.vue';
+import FoodsEntity from '@/entities/FoodsEntity.vue';
+
+const store = useStore();
+
+const playerElement = ref<InstanceType<typeof PlayerEntity> | null>(null);
+const foodsElement = ref<InstanceType<typeof FoodsEntity> | null>(null);
+
+const pixiApp = useApplication();
+
+function checkCollision() {
+    if (!foodsElement.value || !playerElement.value) return;
+    for (let i = 0; i < foodsElement.value.foods.length; i++) {
+        const object = foodsElement.value.foods[i];
+        const rectangleCharacter = (() => {
+            const { x, y, width, height } = playerElement.value.getBounds();
+            return new Rectangle(x, y, width, height);
+        })();
+        const rectangleFoot = (() => {
+            const { x, y, width, height } = object.getBounds();
+            return new Rectangle(x, y, width, height);
+        })();
+        if (rectangleCharacter.intersects(rectangleFoot)) {
+            console.log('Kolizja!');
+            foodsElement.value.foods.splice(i, 1); // Remove the object from the array
+            store.addScore(1);
+        }
+    }
+}
+
+onMounted(() => {
+    pixiApp.value?.ticker.add(checkCollision);
+});
+
+onBeforeUnmount(() => {
+    pixiApp.value?.ticker.remove(checkCollision);
+});
+
+const emit = defineEmits<{
+    (event: 'changeGameState', data: GameState): void;
+}>();
+
+function endGame() {
+    emit('changeGameState', GameState.End);
+}
+
+const { gameOver } = storeToRefs(store);
+
+watch(gameOver, () => {
+    console.log('Game Over!!!!!!!!');
+    endGame();
+});
+</script>
+
+
+
+<style scoped>
+</style>
