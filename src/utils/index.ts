a21cf929d838b87e9dbec5c9de80c06e7f110a7e
Piotr Machacki <piotr@machacki.pl>
2023-06-06 09:39:34 +0200

    .prettierrc

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,16 +8,11 @@ export function getRandomNumber(min: number, max: number): number {
     return Math.random() * (max - min) + min;
 }
 
-export function setNumberInRange(
-    number: number,
-    min: number,
-    max: number
-): number {
+export function setNumberInRange(number: number, min: number, max: number): number {
     let currentNumber = number;
     if (currentNumber < min) currentNumber = min;
     if (currentNumber > max) currentNumber = max;
     return currentNumber;
 }
 
-export const uuid = (prefix = 'uuid'): string =>
-    `${prefix}_${Math.random().toString(36).substring(2)}`;
+export const uuid = (prefix = 'uuid'): string => `${prefix}_${Math.random().toString(36).substring(2)}`;
